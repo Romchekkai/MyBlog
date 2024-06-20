1259eb2c957a50a18412d842bf98cd8a810b2071
Romario123 <kajdenkoroman35052@gmail.com>
2024-06-20 20:12:23 +1000

    remove hadler

--- MyBlog/Program.cs
+++ MyBlog/Program.cs
@@ -77,8 +77,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    //app.UseExceptionHandler("/Home/Error");
-    app.UseStatusCodePagesWithReExecute("/Home/UnexpectedError", "?statusCode={0}");
+    app.UseExceptionHandler("/Home/UnexpectedError");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
